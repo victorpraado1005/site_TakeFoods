356a8eb1fecc3def98c548c020d1cdc89c41931b
Victor Prado <prado.victor1005@gmail.com>
2022-05-03 12:57:16 -0300

    Criação dos links da página e terminando de ajustar a responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,20 @@ let show = true;
 const nav_menu = document.querySelector(".nav_menu")
 const nav_toggle = document.querySelector(".nav_toggle")
 
+
 nav_toggle.addEventListener("click", () => {
 
     document.body.style.overflow = show ? "hidden" : "initial"
 
     nav_menu.classList.toggle("on", show)
     show = !show;
+})
+
+document.querySelectorAll(".nav_toggle_link").forEach(function(link){
+    link.addEventListener("click", ()=>{
+        document.body.style.overflow = show ? "hidden" : "initial"
+
+        nav_menu.classList.toggle("on", show)
+        show = !show;
+    })
 })
