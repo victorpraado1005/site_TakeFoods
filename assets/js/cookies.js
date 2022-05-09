07091c7f8283c0bf796f8c0646c31f02318e758e
Victor Prado <prado.victor1005@gmail.com>
2022-05-08 22:28:08 -0300

    Arquivo cookie.js ajustado com API para buscar o ip do usuario no aceite do cookie message

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -13,15 +13,26 @@ let cookiesHtml = `
 </div>
 `
 
-let lsContent = localStorage.getItem('lgpdCookie')
+let lsContent = localStorage.getItem('CookiesOk')
 if (!lsContent) {
   document.body.innerHTML += cookiesHtml
 
   let cookiesArea = document.querySelector('.cookies-container')
   let cookieButton = document.querySelector('button')
+  let urlAPI = 'https://api.ipify.org?format=json'
 
   cookieButton.addEventListener('click', () => {
     cookiesArea.remove()
-    localStorage.setItem('lgpdCookie', 'Aceito')
+    localStorage.setItem('CookiesOk', 'Ok')
+    fetch(urlAPI)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        let ipAddress = data.ip
+        var data = new Date()
+        console.log(ipAddress)
+        console.log(data)
+      })
   })
 }
